e74a0a1989b1b0ebd38133a25f86520868792e9d
John Tatarowicz <john.tatarowicz@gmail.com>
2020-03-07 14:52:10 -0500

    bleh

--- murdermystery/settings.py
+++ murdermystery/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'murdermystery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bank',
+        'USER': 'DM',
+        'PASSWORD': 'gateway5',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
