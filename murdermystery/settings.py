ba6c18365a12b03b7f895fdf72057a03aea841bb
John Tatarowicz <john.tatarowicz@gmail.com>
2020-03-07 14:40:25 -0500

    allowed_hosts

--- murdermystery/settings.py
+++ murdermystery/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fast-shore-54304.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
